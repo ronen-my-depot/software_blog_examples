f981b5bed18e439cee51048a8a4f5a65a6876158
ronen <ronen@localhost.localdomain>
2015-10-17 22:34:38 +0300

    fix ex4

--- threads/example4_std/thread.cpp
+++ threads/example4_std/thread.cpp
@@ -4,7 +4,7 @@
 #include <boost/lexical_cast.hpp>
 
 
-class Work1{
+class Work{
 	public:
 	void handle(std::string& dataIn){
 		dataIn = boost::lexical_cast<std::string>(std::this_thread::get_id());
@@ -16,13 +16,13 @@ class Work1{
 int main(int argc, char* argv[])
 {
 	std::cout << "main: startup" << std::endl;
-	Work1* work1 = new Work1();
-	Work1* work2 = new Work1();
+	Work* work1 = new Work();
+	Work* work2 = new Work();
 
 	std::string data[2];
 	
-  std::thread worker1(std::bind(&Work1::handle, work1,  std::ref(data[0])));
-  std::thread worker2(std::bind(&Work1::handle, work1,  std::ref(data[1])));  
+  std::thread worker1(std::bind(&Work::handle, work1,  std::ref(data[0])));
+  std::thread worker2(std::bind(&Work::handle, work2,  std::ref(data[1])));  
 
   std::cout << "main: waiting for thread" << std::endl;
   worker1.join();
