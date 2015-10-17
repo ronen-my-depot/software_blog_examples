399f094a43bf8b8ca1539ec51aef01b90d4c76d4
ronen <ronen@localhost.localdomain>
2015-10-18 01:10:01 +0300

    add atomic

---
+++ atomic/atomic_ex1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <atomic>
+
+
+class Work{
+	public:
+		Work(){
+			acc = 0;
+			accAtomic = 0;
+		}
+		void handle(){
+			acc++;
+			accAtomic++;
+		}
+		int acc;
+		std::atomic<int> accAtomic;
+
+};
+
+int main(int argc, char* argv[])
+{
+	enum{NUM_OF_THREADS = 10000};
+	Work* work1 = new Work();
+	std::vector<std::thread*> workers;
+
+	for(int idx = 0; idx < NUM_OF_THREADS; idx++){
+		workers.push_back(new std::thread(std::bind(&Work::handle, work1)));
+	}
+	for(auto worker : workers){
+		worker->join();
+	}
+	// print acc:
+	std::cout << "acc = " << work1->acc << std::endl;
+	std::cout << "accAtomic = " << work1->accAtomic << std::endl;
+
+	
+	for(std::thread* worker : workers){
+		delete worker;
+	}
+	return 0;
+}
